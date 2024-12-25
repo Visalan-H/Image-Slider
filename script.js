132d1000b3b83c131f4d2fb36e280fff38c2dd8f
Visalan <visalanoffl@gmail.com>
2024-12-25 18:19:51 +0530

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector(".slider");
+const max = window.innerWidth / 2;
+const images=Array.from(document.getElementsByClassName("image"));
+console.log(images);
+
+const handleMouseDown = (e) => {
+    slider.dataset.welp = e.clientX;
+};
+
+const handleMouseMove = (e) => {
+    if (parseFloat(slider.dataset.welp) === 0) return;
+
+    let currentPos = parseFloat(slider.dataset.welp) - e.clientX;
+    let currentPerc = (currentPos / max) * -100;
+
+    let finalperc=Math.max(Math.min(parseFloat(slider.dataset.prev)+currentPerc,0),-93);
+    
+    slider.dataset.perc=finalperc;
+    slider.style.transform = `translate(${finalperc}%,-50%)`;
+
+    images.forEach(image => {
+        image.style.objectPosition=`${finalperc+100}% 50%`
+    });
+};
+
+const handleMouseUp = (e) => {
+    slider.dataset.welp = "0";
+    slider.dataset.prev=slider.dataset.perc;
+};
+window.onload=()=>{
+    window.addEventListener("mousedown", (e) => handleMouseDown(e));    
+    window.addEventListener("mousemove", (e) => handleMouseMove(e));
+    window.addEventListener("mouseup", (e) => handleMouseUp(e));
+}
